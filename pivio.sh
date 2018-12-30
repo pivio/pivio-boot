db28be9fa88ef48288dc373c620bc565ea9ee853
Bastian Helfert <bastian.helfert@gmail.com>
2018-12-30 23:05:53 +0100

    Fix possible bugs identified by ShellCheck

--- pivio.sh
+++ pivio.sh
@@ -1,66 +1,66 @@
 #!/bin/sh
 
-OS=`uname`
-HOSTNAME=`hostname`
+OS=$(uname)
+HOSTNAME=$(hostname)
 
 
 # Check if everything we need is there.
 
-if [ $OS != "Linux" ] && [ $OS != "Darwin" ]; then
-  echo "Error: Only Linux or MacOS are supported."
+if [ "$OS" != "Linux" ] && [ "$OS" != "Darwin" ]; then
+  echo Error: Only Linux or MacOS are supported.
   exit 1;
 fi
 
 if ! type "docker" > /dev/null; then
-  echo "Error: You need to have docker installed."
+  echo Error: You need to have docker installed.
   exit 1;
 fi
 
 if ! type "docker-compose" > /dev/null; then
-  echo "Error: You need to have docker-compose installed."
+  echo Error: You need to have docker-compose installed.
   exit 1;
 fi
 
 if ! type "java" > /dev/null; then
-  echo "Error: You need to have java installed."
+  echo Error: You need to have java installed.
   exit 1;
 fi
 
 if ! type "curl" > /dev/null; then
-  echo "Error: You need to have curl installed."
+  echo Error: You need to have curl installed.
   exit 1;
 fi
 
 
 if [ "$OS" = "Darwin" ]; then
 
-   NATIVE_DOCKER_CMD=`docker ps`
-   NATIVE_DOCKER=`echo $?`
+   NATIVE_DOCKER_CMD=$(docker ps)
+   NATIVE_DOCKER=$(echo $?)
 
-  if [ $NATIVE_DOCKER -ne 0 ]; then
-    echo "Check if docker-machine is running."
+  if [ "$NATIVE_DOCKER" -ne 0 ]; then
+    echo Check if docker-machine is running.
 
     if ! type "docker-machine" > /dev/null; then
-      echo "Error: You need to have docker-machine installed."
+      echo Error: You need to have docker-machine installed.
       exit 1;
     fi
 
 
-    DEFAULT_DOCKER_MACHINE=`docker-machine ls | grep default | grep Running | wc -l`
-    if [ $DEFAULT_DOCKER_MACHINE -eq 1 ]; then
-      HOSTNAME=`docker-machine ip default`
+    DEFAULT_DOCKER_MACHINE=$(docker-machine ls | grep default | grep -c Running)
+    if [ "$DEFAULT_DOCKER_MACHINE" -eq 1 ]; then
+      HOSTNAME=$(docker-machine ip default)
     else
       echo "Error: a docker-machine with the name 'default' must be running."
       exit 1;
     fi
 
   else
-    echo "=================================================="
-    echo "You seem like to run docker mac beta. \n PLEASE MAKE SURE YOU HAVE ENABLED THE "
-    echo "\n\n EXPERIMENTAL VPN COMPATIBILITY MODE in the settings. \n\n\n"
-    echo "\n Press >ENTER< to continue \n"
-    echo "=================================================="
-    read
+    echo ==================================================
+    printf "You seem like to run docker mac beta. \n PLEASE MAKE SURE YOU HAVE ENABLED THE "
+    printf "\n\n EXPERIMENTAL VPN COMPATIBILITY MODE in the settings. \n\n\n"
+    printf "\n Press >ENTER< to continue \n"
+    echo ==================================================
+    read -r
   fi
 fi
 
@@ -70,17 +70,17 @@ repos="pivio-web pivio-server pivio-client"
 
 for repo in ${repos}
 do
-   echo $repo
+   echo "$repo"
    if [ -d "$repo" ]
    then
-      cd $repo
+      cd "$repo" || exit
       git pull
       cd ..
    else
-      git clone https://github.com/pivio/$repo.git
+      git clone https://github.com/pivio/"$repo".git
    fi
 
-   cd $repo
+   cd "$repo" || exit
    if [ -e "build.gradle" ]; then
       ./gradlew build --no-daemon
    fi
@@ -144,19 +144,19 @@ echo "Waiting for the servers to come up (on $HOSTNAME). This can take a while b
 # This can take a while because of missing enough entropy. I you know how
 # to speed this up. Let me know. Thanks.
 
-until $(curl --output /dev/null -X GET --silent --head --fail http://$HOSTNAME:9123/document); do
+until curl --output /dev/null -X GET --silent --head --fail http://"$HOSTNAME":9123/document; do
     printf '.'
     sleep 5
 done
 
 if [ $? -eq 0 ]; then
-  echo "Waiting for enough entropy for the webserver to be available."
-  until $(curl --output /dev/null -X GET --silent --head --fail http://$HOSTNAME:8080/); do
+  echo Waiting for enough entropy for the webserver to be available.
+  until curl --output /dev/null -X GET --silent --head --fail http://"$HOSTNAME":8080/; do
       printf '.'
       sleep 5
   done
 
-  echo "Open your webbrowser and point it to $HOSTNAME:8080";
+  echo Open your webbrowser and point it to "$HOSTNAME":8080;
   if [ "$OS" = "Darwin" ]; then
     open "http://$HOSTNAME:8080"
   fi
